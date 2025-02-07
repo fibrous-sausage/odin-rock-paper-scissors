10d42954511449515229e59c473e5797d351773d
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-07 10:04:24 -0500

    Add displays for game status

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const HUMAN_WINS = 0;
+const PLAYER_WINS = 0;
 const COMPUTER_WINS = 1;
 const TIE = 2;
 
@@ -16,22 +16,22 @@ function getComputerChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
+function playRound(playerChoice, computerChoice) {
     // We already sanitize the input above but it can't hurt to do it again.
-    humanChoice.toLowerCase();
+    playerChoice.toLowerCase();
     computerChoice.toLowerCase();
 
-    // 3 cases: human wins, computer wins, it's a tie. Tie results if neither wins.
-    const humanWin = (humanChoice === "rock" && computerChoice === "scissors")
-                || (humanChoice === "paper" && computerChoice === "rock")
-                || (humanChoice === "scissors" && computerChoice === "paper");
+    // 3 cases: player wins, computer wins, it's a tie. Tie results if neither wins.
+    const playerWin = (playerChoice === "rock" && computerChoice === "scissors")
+                || (playerChoice === "paper" && computerChoice === "rock")
+                || (playerChoice === "scissors" && computerChoice === "paper");
 
-    const computerWin = (computerChoice === "rock" && humanChoice === "scissors")
-                || (computerChoice === "paper" && humanChoice === "rock")
-                || (computerChoice === "scissors" && humanChoice === "paper");
+    const computerWin = (computerChoice === "rock" && playerChoice === "scissors")
+                || (computerChoice === "paper" && playerChoice === "rock")
+                || (computerChoice === "scissors" && playerChoice === "paper");
     
-    if (humanWin) {
-        return HUMAN_WINS;
+    if (playerWin) {
+        return PLAYER_WINS;
     } else if (computerWin) {
         return COMPUTER_WINS;
     } else {
@@ -39,39 +39,62 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-let humanScore = 0;
+let playerScore = 0;
 let computerScore = 0;
 let roundsPlayed = 0;
+
+const results = [...document.querySelectorAll("#results p")];
+const gameStateDisplay = results[0];
+const roundPlayedDisplay = results[1];
+const playerScoreDisplay = results[2];
+const computerScoreDisplay = results[3];
+
 
 document.querySelector("#player-choices")
         .addEventListener('click', (event) => {
             if (roundsPlayed >= 5) return;
 
-            const humanChoice = event.target.id;
+            const playerChoice = event.target.id;
             const computerChoice = getComputerChoice();
-            switch(playRound(humanChoice, computerChoice)) {
-                case HUMAN_WINS:
-                    console.log(`You won this round! Computer played ${computerChoice}`);
-                    ++humanScore;
+            const roundResult = playRound(playerChoice, computerChoice);
+
+            switch(roundResult) {
+                case PLAYER_WINS:
+                    ++playerScore;
                     break;
                 case COMPUTER_WINS:
-                    console.log(`Computer won this round! Computer played ${computerChoice}`);
                     ++computerScore;
                     break;
                 case TIE:
-                    console.log(`Round tie! Computer played ${computerChoice}`);
                     break;
             }
 
             ++roundsPlayed;
 
+            playerScoreDisplay.textContent = `Player score: ${playerScore}`;
+            computerScoreDisplay.textContent = `Computer score: ${computerScore}`;
+
             if (roundsPlayed === 5) {
-                if (humanScore > computerScore) {
-                    console.log(`You won! Your score: ${humanScore}, Computer's score: ${computerScore}`);
-                } else if (computerScore < humanScore) {
-                    console.log(`Computer won! Your score: ${humanScore}, Computer's score: ${computerScore}`);
+                roundPlayedDisplay.textContent = `Game over`;
+                if (playerScore > computerScore) {
+                    gameStateDisplay.textContent = `You won!`;
+                } else if (computerScore < playerScore) {
+                    gameStateDisplay.textContent = `Computer won!`;
                 } else {
-                    console.log(`Game tie! Your score: ${humanScore}, Computer's score: ${computerScore}`);
+                    gameStateDisplay.textContent = `Game tie!`;
                 }
+            } else {
+                switch (roundResult) {
+                    case PLAYER_WINS:
+                    gameStateDisplay.textContent = `You won this round!`;
+                    break;
+                case COMPUTER_WINS:
+                    gameStateDisplay.textContent = `Computer won this round!`;
+                    break;
+                case TIE:
+                    gameStateDisplay.textContent = `Round tie!`;
+                    break;
+                }
+                roundPlayedDisplay.textContent = `Round ${roundsPlayed + 1}`;
             }
 });
